07128f4a8ec114c6688c27714aaffd9b88e1c597
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-02-18 20:32:50 +0100

    seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             [
                 TechnologySeeder::class,
                 ProjectSeeder::class,
+                ProjectTechnologySeeder::class
             ]
         );
     }
